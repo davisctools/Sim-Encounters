1dd9f404eb29d3514caf191836a649781b64bad0
davisctools <39494394+davisctools@users.noreply.github.com>
2021-02-08 17:10:37 -0500

    Capitalized the encounter title in the top and sidebar of the Reader.

--- Scripts/SE/Behaviours/EncounterData/Metadata/Display/Label/EncounterTitleLabel.cs
+++ Scripts/SE/Behaviours/EncounterData/Metadata/Display/Label/EncounterTitleLabel.cs
@@ -6,7 +6,9 @@ namespace ClinicalTools.SimEncounters
     [RequireComponent(typeof(TextMeshProUGUI))]
     public class EncounterTitleLabel : EncounterMetadataLabel
     {
+        [SerializeField] private bool capitalizeAll;
+
         protected override void OnMetadataSelected(object sender, EncounterMetadataSelectedEventArgs eventArgs)
-            => Label.text = eventArgs.Metadata.Title;
+            => Label.text = capitalizeAll ? eventArgs.Metadata.Title.ToUpper() : eventArgs.Metadata.Title;
     }
 }
