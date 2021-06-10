3c8af90ef45e8de791150c1f5ac642ac2a6f6fba
davisctools <39494394+davisctools@users.noreply.github.com>
2021-06-10 13:40:12 -0400

    Fix image holder to properly allow preserving images between scenes

--- Scripts/SE/ImageHolder.cs
+++ Scripts/SE/ImageHolder.cs
@@ -1,62 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace ClinicalTools.SimEncounters
 {
     public class ImageHolder : MonoBehaviour
     {
         public static ImageHolder Instance { get; set; }
-        protected List<Sprite> Sprites { get; set; } = new List<Sprite>();
+        protected List<Sprite> Sprites { get; } = new List<Sprite>();
 
-
-        // Start is called before the first frame update
         void Awake()
         {
             if (Instance != null) {
                 Destroy(gameObject);
                 return;
             }
+
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            //SceneManager.sceneLoaded += OnSceneLoaded;
         }
 
         protected int Index { get; set; }
-        // Update is called once per frame
         public static void HoldImage(Sprite sprite)
         {
             if (AcceptsData && Instance != null)
                 Instance.Sprites.Add(sprite);
-            //Instance.Images[Instance.Index++].sprite = sprite;
-            //Instance.Sprites[Instance.Index++] = sprite;
-            //if (Instance.Index >= Instance.Sprites.Length)
-            //Instance.Index = 0;
         }
 
         protected static bool AcceptsData { get; set; }
-        public static void BeginHoldingData()
-        {
-            AcceptsData = true;
-        }
+        public static void BeginHoldingData() => AcceptsData = true;
         public static void StopHoldingData()
         {
             AcceptsData = false;
+            if (Instance != null)
+                Instance.StartCoroutine(Instance.ClearInFrames());
+        }
+
+        protected virtual int ClearFrames { get; } = 5;
+        public IEnumerator ClearInFrames()
+        {
+            for (int i = 0; i < ClearFrames; i++)
+                yield return null;
+
             Instance.Sprites.Clear();
-        }
-
-        public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-        {
-            ClearInFrames();
-        }
-
-        public IEnumerable ClearInFrames()
-        {
-            yield return null;
-            yield return null;
-            yield return null;
         }
     }
 }
