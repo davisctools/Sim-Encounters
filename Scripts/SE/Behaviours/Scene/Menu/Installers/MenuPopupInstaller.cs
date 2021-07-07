6326c80821b173b0e8db031fdcb157a8d68410cd
sophie <39494394+davisctools@users.noreply.github.com>
2021-07-07 14:30:35 -0400

    Move menu installers to an installers folder

---
+++ Scripts/SE/Behaviours/Scene/Menu/Installers/MenuPopupInstaller.cs
@@ -0,0 +1,20 @@
+﻿using ClinicalTools.SimEncounters;
+using UnityEngine;
+using Zenject;
+
+namespace ClinicalTools.ClinicalEncounters
+{
+    public class MenuPopupInstaller : MonoInstaller
+    {
+        public BaseInformationPopup InformationPopup { get => informationPopup; set => informationPopup = value; }
+        [SerializeField] private BaseInformationPopup informationPopup;
+        public BaseInstructionsPopup InstructionsPopup { get => instructionsPopup; set => instructionsPopup = value; }
+        [SerializeField] private BaseInstructionsPopup instructionsPopup;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(InformationPopup);
+            Container.BindInstance(InstructionsPopup);
+        }
+    }
+}
