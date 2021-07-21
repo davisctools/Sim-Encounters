44d3b3be8f5a51f2c8de262bbe603da32eb6f868
sophie <39494394+davisctools@users.noreply.github.com>
2021-07-20 20:40:30 -0400

    UserEncounters are initialized to the last read tab in uncompleted encounters

--- Scripts/SE/Data/Readers/UserEncounter/UserEncounterReader.cs
+++ Scripts/SE/Data/Readers/UserEncounter/UserEncounterReader.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Linq;
+
 namespace ClinicalTools.SimEncounters
 {
     public class UserEncounterReader : IUserEncounterReader
@@ -34,7 +36,68 @@ namespace ClinicalTools.SimEncounters
                 return;
 
             var encounter = new UserEncounter(user, encounterData.Result.Value, detailedStatus.Result.Value);
+            SetEncounterStart(encounter);
+
             result.SetResult(encounter);
+        }
+
+        protected virtual void SetEncounterStart(UserEncounter encounter)
+        {
+            var startPoint = GetStartPoint(encounter);
+            if (startPoint == null)
+                return;
+
+            encounter.Data.Content.NonImageContent.SetCurrentSection(startPoint.Section.Data);
+            startPoint.Section.Data.SetCurrentTab(startPoint.Tab.Data);
+        }
+
+        protected SectionTabPair GetStartPoint(UserEncounter encounter)
+        {
+            UserSection lastSection = null;
+            foreach (var section in encounter.Sections.Values) {
+                if (section.IsRead()) {
+                    lastSection = section;
+                    continue;
+                }
+
+                return GetStartPoint(lastSection, section);
+            }
+
+            return null;
+        }
+
+        protected SectionTabPair GetStartPoint(UserSection lastReadSection, UserSection firstUnreadSection)
+        {
+            UserTab tab = GetLastSequentialTabRead(firstUnreadSection);
+            if (tab != null)
+                return new SectionTabPair(firstUnreadSection, tab);
+            if (lastReadSection == null || lastReadSection.Tabs.ValueArr.Length == 0)
+                return null;
+
+            tab = lastReadSection.Tabs.ValueArr[lastReadSection.Tabs.ValueArr.Length - 1];
+            return new SectionTabPair(lastReadSection, tab);
+        }
+
+        protected UserTab GetLastSequentialTabRead(UserSection section) {
+            UserTab lastTab = null;
+            foreach (var tab in section.Tabs.Values) {
+                if (!tab.IsRead())
+                    return lastTab;
+                lastTab = tab;
+            }
+            return lastTab;
+        }
+
+        protected class SectionTabPair
+        {
+            public UserSection Section { get; set; }
+            public UserTab Tab { get; set; }
+
+            public SectionTabPair(UserSection section, UserTab tab)
+            {
+                Section = section;
+                Tab = tab;
+            }
         }
     }
 }
